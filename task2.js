6593724f42b70024b77f5cfd78182bcde9acc406
=ESTNIZAR <=>
2024-04-29 18:57:12 +0200

    first commit

---
+++ task2.js
@@ -0,0 +1,10 @@
+tasek2//
+async function awaitCall() {
+   
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      const data = await response.json();
+      console.log(data);
+   
+  }
+  
+  awaitCall();
---
+++ task3.js
@@ -0,0 +1,17 @@
+// tasek3
+
+
+
+
+async function awaitCall() {
+  
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  }
+  
+  awaitCall();
